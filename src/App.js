54858a8e463f2a3330a047018f7c6ae81c6c093f
Shivender Singh <104500591+Shivendra437@users.noreply.github.com>
2023-01-07 21:41:33 +0530

    siderbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Sidebar from "./Component/Sidebar";
 import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-    
+    <div>
+      <Sidebar />
     </div>
   );
 }

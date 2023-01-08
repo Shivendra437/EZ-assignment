67981f14d9e4fa38210119e442daa33526a678a3
Shivender Singh <104500591+Shivendra437@users.noreply.github.com>
2023-01-08 12:25:13 +0530

    map function

--- src/Component/Client/Client.jsx
+++ src/Component/Client/Client.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
 import { styled } from "@mui/material/styles";
-import Ezworks from "./Ezworks.jpeg";
+import { Box } from "@mui/material";
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
@@ -10,3 +10,60 @@ const Item = styled(Paper)(({ theme }) => ({
   textAlign: "center",
   color: theme.palette.text.secondary
 }));
+let arr = [
+  { name: "Company name", ccde: "CCDE", en: "8 Entities", user: "64 user" },
+  {
+    name: "company name",
+    ccde: "CCDE",
+    en: "8 Entities",
+    user: "64 user"
+  }
+];
+export default function Client() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        direction: "column",
+        width: "100px",
+        height: "100%"
+      }}
+    >
+      <Stack dirction="column" spacing={1}>
+        {arr.map((el) => (
+          <Item
+            sx={{
+              width: "250px",
+              borderRadius: "15px",
+              "&:hover": {
+                backgroundColor: "grey",
+                cursor: "pointer"
+              }
+            }}
+          >
+            <Box
+              style={{
+                display: "flex",
+                direction: "cloumn",
+                justifyContent: "space-between"
+              }}
+            >
+              <span>CompanyName</span>
+              <span>CCDE</span>
+            </Box>
+            <Box
+              style={{
+                fontSize: "12px",
+                display: "flex",
+                direction: "column",
+                justifyContent: "space-between"
+              }}
+            >
+              <span>8 Entities</span> <span>64 users</span>
+            </Box>
+          </Item>
+        ))}
+      </Stack>
+    </div>
+  );
+}
--- src/Component/Sidebar/Sidebar.jsx
+++ src/Component/Sidebar/Sidebar.jsx
@@ -3,6 +3,7 @@ import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
 import { styled } from "@mui/material/styles";
 import Ezworks from "./Ezworks.jpeg";
+import Client from "../Client/Client";
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
@@ -13,38 +14,40 @@ const Item = styled(Paper)(({ theme }) => ({
 
 export default function ResponsiveStack() {
   return (
-    <div
-      style={{
-        marginLeft: -8,
-        marginTop: -8,
-        backgroundColor: "#0B0B45",
-        width: "70px",
-        height: "960px"
-      }}
-    >
-      <Stack
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        spacing={{ xs: 1, sm: 1, md: 1 }}
+    <>
+      <div
+        style={{
+          marginLeft: -8,
+          marginTop: -8,
+          backgroundColor: "#0B0B45",
+          width: "70px",
+          height: "960px"
+        }}
       >
-        <Item style={{ backgroundColor: "#0B0b45" }}>
-          <img
-            src={Ezworks}
-            alt="Ezworkslogo"
-            style={{ width: "40px", height: "40px" }}
-          />
-        </Item>
-        <Item
-          style={{ width: "20px", height: "20px", backgroundColor: "pink" }}
-        ></Item>
-        <Item
-          style={{ width: "20px", height: "20px", backgroundColor: "pink" }}
-        ></Item>
-        <Item
-          style={{ width: "20px", height: "20px", backgroundColor: "pink" }}
-        ></Item>
-      </Stack>
-    </div>
+        <Stack
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          spacing={{ xs: 1, sm: 1, md: 1 }}
+        >
+          <Item style={{ backgroundColor: "#0B0b45" }}>
+            <img
+              src={Ezworks}
+              alt="Ezworkslogo"
+              style={{ width: "40px", height: "40px" }}
+            />
+          </Item>
+          <Item
+            style={{ width: "20px", height: "20px", backgroundColor: "pink" }}
+          ></Item>
+          <Item
+            style={{ width: "20px", height: "20px", backgroundColor: "pink" }}
+          ></Item>
+          <Item
+            style={{ width: "20px", height: "20px", backgroundColor: "pink" }}
+          ></Item>
+        </Stack>
+      </div>
+    </>
   );
 }

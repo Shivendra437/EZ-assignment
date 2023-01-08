2e62f87de614cc7331a4b0abc8e8d033f18da63b
Shivender Singh <104500591+Shivendra437@users.noreply.github.com>
2023-01-08 11:11:50 +0530

    Update Client.jsx

--- src/Component/Client/Client.jsx
+++ src/Component/Client/Client.jsx
@@ -1,6 +1,12 @@
-import React from "react";
-import { Stack } from "@mui/material";
-
-export default function Client() {
-  return <></>;
-}
+import * as React from "react";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
+import Ezworks from "./Ezworks.jpeg";
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary
+}));

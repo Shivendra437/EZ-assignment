b7f2aac2198608fc82c24f799d3cbd2f67be8198
Shivender Singh <104500591+Shivendra437@users.noreply.github.com>
2023-01-08 11:11:01 +0530

    client

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 // import SideBar from "./Component/Sidebar/Sidebar";
+import Client from "./Component/Client/Client";
 import "./styles.css";
 
 export default function App() {
-  return <div>{/* <SideBar /> */}</div>;
+  return (
+    <div>
+      {/* <SideBar /> */}
+
+      <Client />
+    </div>
+  );
 }
---
+++ src/Component/Client/Client.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Stack } from "@mui/material";
+
+export default function Client() {
+  return <></>;
+}
